b3f1265b5cfcab00311534410658cbff1a47c676
Miharup <74781355+kiharu3112@users.noreply.github.com>
2024-08-14 07:27:55 +0100

    chore:分けた

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { serve } from "@hono/node-server";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
+import { teamHandler } from "./team/main";
 const app = new Hono();
 
 type Team = {
@@ -22,7 +23,7 @@ type Data = {
   team: Team[];
   match: Match[];
 };
-const data: Data = {
+export const data: Data = {
   team: [
     {
       id: "39440930485098",
@@ -40,73 +41,7 @@ app.get("/", (c) => {
   return c.text("Hello Hono");
 });
 
-//   ~~~~~~~~~~~~~~~~~~ チーム ~~~~~~~~~~~~~~~~~~
-
-// チーム一覧の取得
-app.get("/team", (c) => {
-  return c.json(data.team);
-});
-
-// チームの登録
-const PostTeamSchema = z.object({
-  teamName: z.string(),
-  members: z.array(z.string()),
-  isMultiWalk: z.boolean(),
-  category: z.enum(["Elementary", "Primary"]),
-});
-app.post("/team", zValidator("json", PostTeamSchema), (c) => {
-  const team = c.req.valid("json");
-  const id = Math.random().toString(36).slice(-10);
-  data.team.push({ id, ...team, entry: false });
-  return c.json({ message: "Team created successfully" }, 201);
-});
-
-// チームの取得
-app.get("/team/:id", async (c) => {
-  const id = c.req.param("id");
-  const team = data.team.find((team) => team.id === id);
-
-  if (!team) {
-    return c.json({ error: "Team not found" }, 500);
-  }
-  return c.json(team);
-});
-
-// チームの削除
-app.delete("/team/:id", async (c) => {
-  const id = c.req.param("id");
-  const teamExists = data.team.some((team) => team.id === id);
-
-  if (!teamExists) {
-    return c.json({ error: "Team not found" }, 404);
-  }
-  data.team = data.team.filter((team) => team.id !== id);
-  return c.json({ message: "Team deleted successfully" }, 200);
-});
-
-// エントリー登録
-app.post("/team/:id/entry", async (c) => {
-  const id = c.req.param("id");
-  const team = data.team.find((team) => team.id === id);
-
-  if (!team) {
-    return c.json({ error: "Team not found" }, 404);
-  }
-  team.entry = true;
-  return c.json({ message: "Entry registered successfully" });
-});
-
-// エントリー削除
-app.delete("/team/:id/entry", async (c) => {
-  const id = c.req.param("id");
-  const team = data.team.find((team) => team.id === id);
-
-  if (!team) {
-    return c.json({ error: "Team not found" }, 404);
-  }
-  team.entry = false;
-  return c.json({ message: "Entry deleted successfully" });
-});
+app.route("/team", teamHandler);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
---
+++ src/team/main.ts
@@ -0,0 +1,72 @@
+import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { data } from "../index";
+
+export const teamHandler = new Hono();
+
+// チーム一覧の取得
+teamHandler.get("/", (c) => {
+  return c.json(data.team);
+});
+
+// チームの登録
+const PostTeamSchema = z.object({
+  teamName: z.string(),
+  members: z.array(z.string()),
+  isMultiWalk: z.boolean(),
+  category: z.enum(["Elementary", "Primary"]),
+});
+teamHandler.post("/", zValidator("json", PostTeamSchema), (c) => {
+  const team = c.req.valid("json");
+  const id = Math.random().toString(36).slice(-10);
+  data.team.push({ id, ...team, entry: false });
+  return c.json({ message: "Team created successfully" }, 201);
+});
+
+// チームの取得
+teamHandler.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const team = data.team.find((team) => team.id === id);
+
+  if (!team) {
+    return c.json({ error: "Team not found" }, 500);
+  }
+  return c.json(team);
+});
+
+// チームの削除
+teamHandler.delete("/:id", async (c) => {
+  const id = c.req.param("id");
+  const teamExists = data.team.some((team) => team.id === id);
+
+  if (!teamExists) {
+    return c.json({ error: "Team not found" }, 404);
+  }
+  data.team = data.team.filter((team) => team.id !== id);
+  return c.json({ message: "Team deleted successfully" }, 200);
+});
+
+// エントリー登録
+teamHandler.post("/:id/entry", async (c) => {
+  const id = c.req.param("id");
+  const team = data.team.find((team) => team.id === id);
+
+  if (!team) {
+    return c.json({ error: "Team not found" }, 404);
+  }
+  team.entry = true;
+  return c.json({ message: "Entry registered successfully" });
+});
+
+// エントリー削除
+teamHandler.delete("/:id/entry", async (c) => {
+  const id = c.req.param("id");
+  const team = data.team.find((team) => team.id === id);
+
+  if (!team) {
+    return c.json({ error: "Team not found" }, 404);
+  }
+  team.entry = false;
+  return c.json({ message: "Entry deleted successfully" });
+});
